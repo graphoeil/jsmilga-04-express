55e4884afd2f0cab00884ee6af64732b8f0a76e0
graphoeil <graphoeil@gmail.com>
2022-12-25 13:05:40 +0100

    10-express-params-queryString-setup.js

--- app.js
+++ app.js
@@ -16,12 +16,36 @@ app.get('/', (req, res) => {
 	`);
 });
 app.get('/api/products', (req, res) => {
-	// Show products without description
+	// Show products without price and description
 	const newProducts = products.map((product) => {
 		const { id, name, image } = product;
 		return { id, name, image };
 	});
 	res.json(newProducts);
+	/*
+	[
+		{
+			"id": 1,
+			"name": "albany sofa",
+			"image": "https://dl.airtable.com/.attachments/6ac7f7b55d505057317534722e5a9f03/9183491e/product-3.jpg"
+		},
+		{
+			"id": 2,
+			"name": "entertainment center",
+			"image": "https://dl.airtable.com/.attachments/da5e17fd71f50578d525dd5f596e407e/d5e88ac8/product-2.jpg"
+		},
+		{
+			"id": 3,
+			"name": "albany sectional",
+			"image": "https://dl.airtable.com/.attachments/05ecddf7ac8d581ecc3f7922415e7907/a4242abc/product-1.jpeg"
+		},
+		{
+			"id": 4,
+			"name": "leather sofa",
+			"image": "https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg"
+		}
+	]
+	*/
 });
 
 // Server listen
